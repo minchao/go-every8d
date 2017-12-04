b6aea2ebe99b6d7c0d8cd6ec5fbb0251255d76bc
minchao <minchao.220@gmail.com>
2017-12-04 23:42:10 +0800

    Add every8d_test.go

---
+++ every8d_test.go
@@ -0,0 +1,53 @@
+package every8d
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+var (
+	// mux is the HTTP request multiplexer used with the test server.
+	mux *http.ServeMux
+
+	// client is the Apple Music client being tested.
+	client *Client
+
+	// server is a test HTTP server used to provide mock API responses.
+	server *httptest.Server
+)
+
+// setup sets up a test HTTP server along with a every9d.Client that is configured to talk to that test server.
+func setup() {
+	// test server
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+
+	// EVERY8D client configured to use test server
+	client = NewClient("username", "password", nil)
+	u, _ := url.Parse(server.URL)
+	client.BaseURL = u
+}
+
+// teardown closes the test HTTP server.
+func teardown() {
+	server.Close()
+}
+
+func testMethod(t *testing.T, r *http.Request, want string) {
+	if got := r.Method; got != want {
+		t.Errorf("Request method: %v, want %v", got, want)
+	}
+}
+
+func TestNewClient(t *testing.T) {
+	c := NewClient("username", "password", nil)
+
+	if got, want := c.BaseURL.String(), defaultBaseURL; got != want {
+		t.Errorf("NewClient BaseURL is %v, want %v", got, want)
+	}
+	if got, want := c.UserAgent, defaultUserAgent; got != want {
+		t.Errorf("NewClient UserAgent is %v, want %v", got, want)
+	}
+}
