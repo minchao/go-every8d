59cb09d5c791f65a44ab8da7665fa22687187de3
minchao <minchao.220@gmail.com>
2017-12-09 21:24:37 +0800

    Add GetCredit()

---
+++ credit.go
@@ -0,0 +1,39 @@
+package every8d
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"net/url"
+	"strconv"
+)
+
+// GetCredit retrieves your account balance.
+func (c *Client) GetCredit(ctx context.Context) (float64, error) {
+	u, _ := url.Parse("API21/HTTP/getCredit.ashx")
+
+	req, err := c.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return 0.0, err
+	}
+
+	fn := func(body io.Reader, v interface{}) error {
+		result, _ := ioutil.ReadAll(body)
+		credit, err := strconv.ParseFloat(string(result), 64)
+		if err != nil {
+			return err
+		}
+
+		*v.(*float64) = credit
+
+		return nil
+	}
+
+	credit := new(float64)
+	_, err = c.Do(ctx, req, fn, credit)
+	if err != nil {
+		return 0.0, err
+	}
+
+	return *credit, nil
+}
