2a28e909eaa12f27c2818525b532b8743585f891
minchao <minchao.220@gmail.com>
2017-12-18 23:45:10 +0800

    Implement GetDeliveryStatus()

--- delivery_status.go
+++ delivery_status.go
@@ -2,21 +2,35 @@ package every8d
 
 import (
 	"context"
+	"encoding/csv"
 	"io"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
+// DeliveryStatus
 type DeliveryStatus struct {
-	Count int
+	Name     string
+	Mobile   string
+	SendTime string
+	Cost     float64
+	Status   StatusCode
+}
+
+// DeliveryStatusResponse represents the response of get delivery status.
+type DeliveryStatusResponse struct {
+	Count   int
+	Records []DeliveryStatus
 }
 
 // GetDeliveryStatus retrieves the delivery status.
-func (c *Client) GetDeliveryStatus(ctx context.Context, batchID, pageNo string) (*DeliveryStatus, error) {
+func (c *Client) GetDeliveryStatus(ctx context.Context, batchID, pageNo string) (*DeliveryStatusResponse, error) {
 	q := url.Values{}
 	q.Set("BID", batchID)
 	q.Set("PNO", pageNo)
 	u, _ := url.Parse("API21/HTTP/getDeliveryStatus.ashx")
+	u.RawQuery = q.Encode()
 
 	req, err := c.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
@@ -24,11 +38,37 @@ func (c *Client) GetDeliveryStatus(ctx context.Context, batchID, pageNo string) 
 	}
 
 	fn := func(body io.Reader, v interface{}) error {
-		// TODO
+		response := DeliveryStatusResponse{}
+
+		r := csv.NewReader(body)
+		r.Comma = '\t'
+
+		if record, err := r.Read(); err == nil && len(record) == 1 {
+			response.Count, _ = strconv.Atoi(record[0])
+		}
+		for {
+			record, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+
+			cost, _ := strconv.ParseFloat(record[3], 64)
+			status, _ := strconv.Atoi(record[4])
+			response.Records = append(response.Records, DeliveryStatus{
+				Name:     record[0],
+				Mobile:   record[1],
+				SendTime: record[2],
+				Cost:     cost,
+				Status:   StatusCode(status),
+			})
+		}
+
+		*v.(*DeliveryStatusResponse) = response
+
 		return nil
 	}
 
-	result := new(DeliveryStatus)
+	result := new(DeliveryStatusResponse)
 	_, err = c.Do(ctx, req, fn, result)
 	if err != nil {
 		return nil, err
---
+++ delivery_status_test.go
@@ -0,0 +1,53 @@
+package every8d
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestClient_GetDeliveryStatus(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/API21/HTTP/getDeliveryStatus.ashx", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{
+			"BID": "00000000-0000-0000-0000-000000000000",
+			"PNO": "1",
+		})
+		fmt.Fprint(w, `2
+Test	+886987654321	2017/12/18 23:14:17	1	100
+	+886987654321	2017/12/18 23:14:18	0	101`)
+	})
+
+	want := &DeliveryStatusResponse{
+		Count: 2,
+		Records: []DeliveryStatus{
+			{
+				Name:     "Test",
+				Mobile:   "+886987654321",
+				SendTime: "2017/12/18 23:14:17",
+				Cost:     1,
+				Status:   StatusCode(100),
+			},
+			{
+				Name:     "",
+				Mobile:   "+886987654321",
+				SendTime: "2017/12/18 23:14:18",
+				Cost:     0,
+				Status:   StatusCode(101),
+			},
+		},
+	}
+
+	got, err := client.GetDeliveryStatus(context.Background(), "00000000-0000-0000-0000-000000000000", "1")
+	if err != nil {
+		t.Errorf("GetDeliveryStatus returned unexpected error: %v", err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("GetDeliveryStatus returned %+v, want %+v", got, want)
+	}
+}
