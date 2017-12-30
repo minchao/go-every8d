698f1b21f8c1ebff4e9e5ea61f2c305cb9f2d54e
minchao <minchao.220@gmail.com>
2017-12-30 15:06:33 +0800

    The MMS subject should be required

--- send.go
+++ send.go
@@ -62,7 +62,28 @@ func (c *Client) Send(ctx context.Context, message Message) (*SendResponse, erro
 
 // MMS represents an MMS object.
 type MMS struct {
-	Message
+	// Message title.
+	Subject string `form:"SB"`
+
+	// Message content.
+	Content string `form:"MSG"`
+
+	// Receiver's mobile number.
+	// Format: +88612345678 or 0912345678
+	// Separator: (,) e.g. 0912345678,0922333444
+	Destination string `form:"DEST"`
+
+	// Reservation time
+	// Send immediately: No input (empty).
+	// Reservation send:Please input the reservation time, using this format: yyyyMMddHHmnss, e.g. 20090131153000
+	ReservationTime string `form:"ST,omitempty"`
+
+	// SMS validity period of unit: minutes.
+	// if not specified, then the platform default validity period is 1440 minutes.
+	RetryTime int `form:"RETRYTIME,omitempty"`
+
+	// Message record no.
+	MessageNo string `form:"MR,omitempty"`
 
 	// Image file, binary base64 encoded.
 	Attachment string `form:"ATTACHMENT"`
